36713e73b37faf3ce5464db4af46c197057bbb34
devangag558 <76725218+devangag558@users.noreply.github.com>
2021-06-06 18:13:52 +0530

    Update Note.jsx

--- Note.jsx
+++ Note.jsx
@@ -1,10 +1,13 @@
 import React from "react";
-
 function Note() {
   return (
     <div className="note">
       <h1>Javascript and React.js</h1>
-      <p>fhbvfdkvbfdjkvbkdfjvbfdjkvbdfj</p>
+      <p>
+        JavaScript is high-level, often just-in-time compiled, and
+        multi-paradigm. It has curly-bracket syntax, dynamic typing,
+        prototype-based object-orientation, and first-class functions.{" "}
+      </p>
     </div>
   );
 }
